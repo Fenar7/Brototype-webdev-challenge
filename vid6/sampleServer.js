35ddc38c1f08649f319d7f7a6dac787d6f064429
Fenar Khan <fenarkhan7@gmail.com>
2023-10-09 14:35:14 +0530

    vid 7 started

--- vid6/sampleServer.js
+++ vid6/sampleServer.js
@@ -11,9 +11,25 @@ var http = require('http')
 var fs = require('fs')
 
 http.createServer(function (req,res){
-    fs.readFile('hello.html',function(err,data){ // this functin is called when the html files has completed reading
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
+
+    if(req.url==="/"){
+        fs.readFile('hello.html',function(err,data){ // this functin is called when the html files has completed reading
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        })
+    }
+    else if(req.url==="/signup"){
+        fs.readFile('signup.html', (err,data)=>{
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            res.end
+        })
+    }
+    else{
+        res.writeHead(404,{'Content-Type':'text/html'})
         res.end()
-    })
-}).listen(7000)
+    }
+}).listen(7000,function(){
+    console.log('Server Started')
+})
