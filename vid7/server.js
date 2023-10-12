f1d2167f772f4972dccc2d8d6f037914722ac7ef
Fenar Khan <fenarkhan7@gmail.com>
2023-10-12 14:14:04 +0530

    jfgkdfjg

--- vid7/server.js
+++ vid7/server.js
@@ -1,27 +1,33 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 
 http.createServer(function (req,res){
-    if(req.url==='/'){
+
+    var q = url.parse(req.url,true) // if we set to true we get the vbalues as onjects from passicng the html page
+    console.log(q.pathname)
+
+    if(q.pathname==='/'){
         fs.readFile('sample.html',function (err,hai){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(hai)
             res.end()
         })
     }
-    else if(req.url==='/login'){
+    else if(q.pathname==='/login'){
         fs.readFile('login.html',function (err,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
     }
-    else if(req.url==='/loginaction'){
-        fs.readFile('loginaction.html', function(err,data){
-            res.writeHead(200,{'Content-Type':'text/css'})
-            res.write(data)
-            res.end() 
-        })
+    else if(q.pathname==='/loginaction'){
+        //to get the data
+        res.writeHead(200,{'Content-Type':'text/html'})
+        console.log(q.query)
+        console.log(q.query.lname)
+        res.write('<h1>'+"HI "+q.query.fname+'</h1>')
+        res.end()
     }
     else{
         res.writeHead(404,{'Content-Type' : 'text/html'})
